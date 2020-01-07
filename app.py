8d89c3977ec5828efdf7e1ef8041e0bf8340b8c1
fwmoor <fwmoor@outlook.com>
2020-01-07 11:03:00 +0200

    started with basic register

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, url_for
-from forms import RegistrationForm
 app = Flask(__name__)
 
 post = [
@@ -13,28 +12,18 @@ post = [
 	}
 ]
 
-
 @app.route('/')
 @app.route('/home')
 def home():
 	return render_template('home.html', posts=post)
 
-
 @app.route('/about')
 def about():
 	return render_template('about.html', title='About')
-
 
 @app.route('/register')
 def register():
-	form = RegistrationForm()
-	return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/about')
-def about():
-	return render_template('about.html', title='About')
-
+	return render_template('register.html', title='Register')
 
 if __name__ == '__main__':
 	app.run()
