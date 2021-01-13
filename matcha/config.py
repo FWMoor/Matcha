196b4279d3d948a2fac7b3fc317b9af5276c0f7c
fwmoor <fwmoor@outlook.com>
2021-01-13 12:43:09 +0200

    fixy fixy

--- matcha/config.py
+++ matcha/config.py
@@ -14,20 +14,20 @@ class Config:
 	"""
 
 	
-	# export MAIL_USERNAME="Your email"
-	# export MAIL_PASSWORD="Your email password"
-	# export SECRET_KEY="YOUR SUPERSECRET PASSWORD"
-	# export UPLOAD_FOLDER="./photos"
-	# export MAIL_SERVER="smtp.gmail.com"
-	# export MAIL_PORT=465
-	# export MAIL_USE_SSL=True
-	# export SESSION_COOKIE_SAMESITE="Lax"
+	MAIL_USERNAME="projectmatcha60@gmail.com"
+	MAIL_PASSWORD="matcha123"
+	SECRET_KEY="secrets.token_urlsafe(16)"
+	UPLOAD_FOLDER="./photos"
+	MAIL_SERVER="smtp.gmail.com"
+	MAIL_PORT=465
+	MAIL_USE_SSL=True
+	SESSION_COOKIE_SAMESITE="Lax"
 	
-	SECRET_KEY = os.environ.get('SECRET_KEY')
-	UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-	MAIL_SERVER = os.environ.get('MAIL_SERVER')
-	MAIL_PORT = os.environ.get('MAIL_PORT')
-	MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	SESSION_COOKIE_SAMESITE = os.environ.get('SESSION_COOKIE_SAMESITE')
+	# SECRET_KEY = os.environ.get('SECRET_KEY')
+	# UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
+	# MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	# MAIL_PORT = os.environ.get('MAIL_PORT')
+	# MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+	# MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	# MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	# SESSION_COOKIE_SAMESITE = os.environ.get('SESSION_COOKIE_SAMESITE')
--- matcha/db.py
+++ matcha/db.py
@@ -15,13 +15,14 @@ def dict_factory(cursor, row):
 	return d
 
 def setup_tables():
-	con = db_connect()
-	cur = con.cursor()
-	with open(SQL_PATH, 'r') as sqlfile:
-		sql = sqlfile.read()
-	cur.executescript(sql)
-	# Add admin account
-	cur.execute('INSERT INTO users (fname, lname, username, email, password, verify) VALUES (?, ?, ?, ?, ?, ?)', 
-	['System', 'System', 'system', 'System@mailcatch.com', '70d6d3db2b8cee727994e89f9b8c21622e39840ad579dd82da37aadd441473aab9996dd749d652b8023791f3862ca3cc584f9ff9c27222217e77af241d3b3abd54486eeb78c733c57aab7aa7ff5709ec90655dee193c4a32e46ffb2796049d0b', None])
-	con.commit()
-	con.close()
+	if not(os.path.exists(DEFAULT_PATH)):
+		con = db_connect()
+		cur = con.cursor()
+		with open(SQL_PATH, 'r') as sqlfile:
+			sql = sqlfile.read()
+		cur.executescript(sql)
+		# Add admin account
+		cur.execute('INSERT INTO users (fname, lname, username, email, password, verify) VALUES (?, ?, ?, ?, ?, ?)', 
+		['System', 'System', 'system', 'System@mailcatch.com', '70d6d3db2b8cee727994e89f9b8c21622e39840ad579dd82da37aadd441473aab9996dd749d652b8023791f3862ca3cc584f9ff9c27222217e77af241d3b3abd54486eeb78c733c57aab7aa7ff5709ec90655dee193c4a32e46ffb2796049d0b', None])
+		con.commit()
+		con.close()
